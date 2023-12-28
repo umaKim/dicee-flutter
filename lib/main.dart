0ba7cea2de8ae20172ceb19a83fdaca4e37c62e9
UmaKim <85341050+umaKim@users.noreply.github.com>
2023-12-28 20:21:58 +0900

    dice app 구현

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'dart:math';
 
 void main() {
   return runApp(
@@ -15,9 +16,58 @@ void main() {
   );
 }
 
-class DicePage extends StatelessWidget {
+//stateful
+// state does not change
+//
+
+//stateless
+// state changes
+
+// final class DicePage extends StatelessWidget {
+//
+// }
+
+class DicePage extends StatefulWidget {
+  @override
+  _DicePageState createState() => _DicePageState();
+}
+
+class _DicePageState extends State<DicePage> {
+  int leftNumber = 1;
+  int rightNumber = 5;
+
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Center(
+      child: Row(
+        children: [
+          Expanded(
+            child: TextButton(
+              onPressed: () {
+                setState(() {
+                  diceRoll();
+                });
+              },
+              child: Image.asset('images/dice$leftNumber.png'),
+            ),
+          ),
+          Expanded(
+            child: TextButton(
+              onPressed: () {
+                setState(() {
+                  diceRoll();
+                });
+              },
+              child: Image.asset('images/dice$rightNumber.png'),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+
+  void diceRoll() {
+    leftNumber = Random().nextInt(6) + 1;
+    rightNumber = Random().nextInt(6) + 1;
   }
 }
